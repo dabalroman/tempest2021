707c214fa9248dfc03f6323c131d37f88ad8d7e0
Roman Dąbal <dabalroman@gmail.com>
2021-05-31 19:47:31 +0200

    Removed unnecessary Vue components; Plan JS test;

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue';
-import App from './App.vue';
+import * as THREE from 'three';
 
-Vue.config.productionTip = false;
+// eslint-disable-next-line no-unused-vars
+const scene = new THREE.Scene();
+// eslint-disable-next-line no-unused-vars
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-new Vue({
-    render: h => h(App),
-}).$mount('#app');
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+}
+animate();
