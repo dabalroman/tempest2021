3d8b2f2b602dbe5a112fce4ad95f0421f89ee53c
Peterka15 <pklosek@onet.pl>
2021-06-06 01:16:03 +0200

    activeLane setting added

--- src/Object/Surface.js
+++ src/Object/Surface.js
@@ -1,4 +1,4 @@
-import { Vector2 } from 'three';
+import {Vector2} from 'three';
 import BoundingBox2 from '@/Helpers/BoundingBox2';
 
 export default class Surface {
@@ -12,31 +12,40 @@ export default class Surface {
   lanesCoords;
   /** @var {Vector2[]} lanesCoords */
   centeredLanesCoords;
+  /** @var {number} activeLane */
+  activeLane;
 
   /**
    * @param {string} name
    * @param {boolean} isOpen
    * @param {Vector2[]} lanesCoords
    */
-  constructor (name, isOpen, lanesCoords) {
+  constructor(name, isOpen, lanesCoords) {
     this.name = name;
     this.isOpen = isOpen;
     this.lanesCoords = lanesCoords;
     this.lanesAmount = lanesCoords.length - (isOpen ? 1 : 0);
+    this.activeLane = 0;
 
     this.calculateCenteredLanesCoords();
   }
 
-  calculateCenteredLanesCoords () {
+  calculateCenteredLanesCoords() {
     let boundingBox2 = BoundingBox2.create(this.lanesCoords);
-    this.centeredLanesCoords =  this.lanesCoords.map(vector2 => vector2.sub(boundingBox2.getCenter()));
+    this.centeredLanesCoords = this.lanesCoords.map(vector2 => vector2.sub(boundingBox2.getCenter()));
+  }
+
+  setActiveLane(newActiveID) {
+    if (newActiveID >= 0 && newActiveID <= this.lanesAmount) {
+      this.activeLane = newActiveID;
+    }
   }
 
   /**
    * @param {{name:string, isOpen: boolean, coords:{x: number, y: number}[]}[]} dataset
    * @return {Surface[]}
    */
-  static fromDataset (dataset) {
+  static fromDataset(dataset) {
     return dataset.map(data =>
       new Surface(
         data.name,
